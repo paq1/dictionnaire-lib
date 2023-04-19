2536e31e95cbf8da16261a75311288f5c9cb59fa
Pierre Paquin <p.paquin1016@laposte.net>
2023-04-19 21:30:43 +0200

    :bug: fix: changement du chargement

--- src/services/loader.rs
+++ src/services/loader.rs
@@ -2,18 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub(crate) fn load_words() -> Vec<String> {
-    File::open("./data/french.txt")
-        .map(|file| {
-            let reader = BufReader::new(file);
-
-            reader
-                .lines()
-                .into_iter()
-                .map(|line| {
-                    line.unwrap_or("".to_string())
-                })
-                .filter(|word| !word.trim().is_empty())
-                .collect::<Vec<_>>()
-        })
-        .unwrap_or(vec![])
+    let data = include_str!("../../data/french.txt");
+    data
+        .split("\n")
+        .map(|e| e.to_string())
+        .collect::<Vec<_>>()
 }
